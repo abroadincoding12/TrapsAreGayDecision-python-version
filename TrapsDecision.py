8f29a2ae3f76d9a4134b0a9c46d36e4055fde00a
Iree McGirt <theradgamerivan@gmail.com>
2018-03-14 13:48:22 -0400

    Add files via upload

---
+++ TrapsDecision.py
@@ -0,0 +1,12 @@
+traps = "it depends"
+
+if traps == "gay":
+	print("Traps are gay.")
+elif traps == "not gay":
+	print("Traps are not gay.")
+elif traps == "it depends":
+	print("I guess your right.")
+else:
+	print("Come back when you thought of a decision.")
+	
+"This code works so use it if you want"
